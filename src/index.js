3db2804f1944ef07dc7a5b9ad93d9758d7c6bed6
Christopher Dougherty <christophertdoc@gmail>
2017-08-31 08:53:47 -0400

    add firebase

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import * as firebase from 'firebase';
+
+var config = {
+    apiKey: "AIzaSyD7Cj4P1VolSooKeCKhfeYwLxP3mjb01uk",
+    authDomain: "react-blocitoff.firebaseapp.com",
+    databaseURL: "https://react-blocitoff.firebaseio.com",
+    projectId: "react-blocitoff",
+    storageBucket: "react-blocitoff.appspot.com",
+    messagingSenderId: "801994369004"
+  };
+  firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker(); 
+registerServiceWorker();
