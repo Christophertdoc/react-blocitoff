27f9da7ba59a9a6a4ab58f7b40ddfbca21c73aa2
Christopher Dougherty <christophertdoc@gmail>
2017-09-01 07:36:52 -0400

    add firebase

--- src/App.js
+++ src/App.js
@@ -13,26 +13,19 @@ constructor() {
 }
 
 componentDidMount() {
-    const rootRef = firebase.database().ref().child('react');
+    const rootRef = firebase.database().ref();
     const speedRef = rootRef.child('speed');
     speedRef.on('value', snap => {
       this.setState({
           speed: snap.val()
-      })
+      });
     });
 }
 
   render() {
     return (
       <div className="App">
-        <h1>{this.state.speed}</h1>
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <h1>{this.state.speed}</h1>
       </div>
     );
   }
