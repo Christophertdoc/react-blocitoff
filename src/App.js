5c3d5221cb1953f94115f0122cf515fc172f4263
Alissa Likavec <alissalikavec@gmail.com>
2017-09-29 16:24:57 -0700

    Meeting notes

--- src/App.js
+++ src/App.js
@@ -7,17 +7,19 @@ import * as firebase from 'firebase';
 class App extends Component {
   constructor(props) {
       super(props);
+      this.firebase = firebase.database().ref('tasks')
       this.state = {
-          tasks: []
+          tasks: [],
+          // db: firebase.database().ref('tasks')
       };
 
   }
   updateTasks(){
-    let tasksRef = firebase.database().ref('tasks').orderByKey().limitToLast(100);
+    let tasksRef = this.firebase.orderByKey().limitToLast(100);
     tasksRef.on('child_added', snapshot => {
         let task = snapshot.val();
         if(Date.now() - task.createdAt >= 120000){
-          firebase.database().ref('tasks').child(snapshot.key).update({
+          this.firebase.child(snapshot.key).update({
               expired: true
           });
         }
@@ -34,21 +36,21 @@ class App extends Component {
   }
 
   addTask(e) {
-    let input = this.inputEl.value;
-    let inputPriority = this.inputPriority.value;
-    if(input.length > 0 &&
-       (inputPriority === "low" || inputPriority === "med" || inputPriority === "high")){
+    let input = this.inputEl;
+    let dropDownPriority = this.dropDownPriority;
+    if(dropDownPriority){
       e.preventDefault();
       let todo = {
-        text: input,
+        text: input.value,
         createdAt: Date.now(),
         completed: false,
         expired: false,
-        priorityLevel: inputPriority,
+        priorityLevel: dropDownPriority.value,
       }
-      firebase.database().ref('tasks').push( todo );
-      input = '';
-      inputPriority = '';
+      input.value = '';
+      dropDownPriority.value = "low"
+      this.firebase.push( todo );
+
     }
     else {
       alert("Please make sure task field is filled, and that the task has a priority of low, med, or high.");
@@ -57,7 +59,7 @@ class App extends Component {
   completeTask(task){
     // mark a task's completed field as true
     // then push to firebase
-    firebase.database().ref('tasks').child(task.id).update({
+    this.firebase.child(task.id).update({
         completed: true
     });
     task.completed = true;
@@ -71,7 +73,11 @@ class App extends Component {
           <h4>Enter Task</h4>
           <input type="text" ref={ el => this.inputEl = el }/>
           <h4>Enter Priority Level (low, med, high)</h4>
-          <input type="text" ref={ el => this.inputPriority = el }/>
+          <select name="Select a priority" ref={ el => this.dropDownPriority = el }>
+            <option value="low">Low</option>
+            <option value="medium">Medium</option>
+            <option value="high">High</option>
+            </select>
           <input type="submit"/>
 
           <h2>Active Tasks</h2>
